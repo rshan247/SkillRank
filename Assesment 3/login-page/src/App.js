9092f15d3226ec0f3c9589acbfaa36fd4f9d7e9f
unknown <rshan007247@gmail.com>
2024-05-07 21:16:34 +0530

    Created Login page for assesment 3 using react

---
+++ Assesment 3/login-page/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Login from './Components/Login';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
